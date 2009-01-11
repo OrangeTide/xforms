c671499b41cd2c748222f6ce52d1e192683cc2cc
Jens Thoms Törring <jt@toerring.de>
2009-01-11 20:54:14 +0000

    See ChangeLog from 2009/01/11

--- demos/thumbwheel.c
+++ demos/thumbwheel.c
@@ -52,7 +52,7 @@ returnchange_cb( FL_OBJECT * ob,
 				 long        data  FL_UNUSED_ARG )
 {
      FD_twheelform *fdui = ( FD_twheelform * ) ob->form->fdui;
-     int n = fl_get_choice( ob ) - 1;
+     int n = fl_get_select_item( ob )->val;
 
      fl_set_thumbwheel_return( fdui->vert, n );
      fl_set_thumbwheel_return( fdui->hor, n );
